269b9d6a5b072596ab1db9ded4b663949f11dab9
Surender Kumar <111198040+monudisodia@users.noreply.github.com>
2022-11-15 21:53:38 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,11 @@ import React from 'react'
 import '../styles/App.css';
 const App = () => {
 
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    console.log("form submitted");
+    
+  }
 
   return (
     <div id="main">
@@ -11,7 +16,7 @@ const App = () => {
 
         <br />
         <br />
-        <button type='submit'>Submit</button>
+        <button type='submit' onClick={handleSubmit}>Submit</button>
       </form>
     </div>
   )
